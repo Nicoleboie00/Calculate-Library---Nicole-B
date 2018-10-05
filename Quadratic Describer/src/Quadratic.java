d23fcd0df4b7b782aadeb8fbb37d5dcbd690742c
APCS3rd <APCS3rd@MHS-Dreyer-02>
2018-10-05 10:58:40 -0700

    new part 5

---
+++ Quadratic Describer/src/Quadratic.java
@@ -0,0 +1,20 @@
+/*This class is reponsible for all calculations
+ * @author Nicole Boie
+ * @Version October 5, 2018
+ */
+public class Quadratic {
+	public static String quadrDescriber(double a, double b, double c) {
+		//opens
+		if (a>0) {
+			String open = ("Opens:" + "Up");	
+		}else {
+			String open= ("Opens:" + "Down");	
+	//do i have to find if a = 0
+		}
+		//axis of symmetry
+		double line = ((-b)/(2*a));
+			String aos = ("Axis of Symmetry:" + line);
+		//vertex
+			
+	}
+}
---
+++ Quadratic Describer/src/QuadraticClient.java
@@ -0,0 +1,9 @@
+/*This class is client code that handles interactions with user and call quadrDescriber
+ * @author Nicole Boie
+ * @Version October 5, 2018
+ */
+public class QuadraticClient {
+	public static void main(String[] args) {
+		
+	}
+}
